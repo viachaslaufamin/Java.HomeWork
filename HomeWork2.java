fc44e6b43cfe72500e30fc8aa1e89b449586e0c9
viachaslaufamin <119659118+viachaslaufamin@users.noreply.github.com>
2023-06-12 20:37:08 +0200

    Add files via upload

---
+++ HomeWork2.java
@@ -0,0 +1,14 @@
+package homeWork;
+
+public class HomeWork2 {
+    public static void main(String[] args) {
+       double w = (float) Math.pow(Math.E, 5) + 1.5;
+        System.out.println(w);
+        int r = (byte) Math.log(2) + (short) 14;
+        System.out.println(r);
+        double f = 1 + 1.5;
+        System.out.println(f);
+
+
+    }
+}
