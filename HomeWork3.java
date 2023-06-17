ee15e29eb96304f88c77d428421bf2db91e1c683
viachaslaufamin <119659118+viachaslaufamin@users.noreply.github.com>
2023-06-17 02:13:58 +0200

    Add files via upload

---
+++ HomeWork3.java
@@ -0,0 +1,26 @@
+package homeWork;
+
+public class HomeWork3 {
+    public static void main(String[] args) {
+        char x = '0';
+        char w = '0';
+        char r = '0';
+
+
+        int t = x;
+        System.out.println(t);
+
+        String y = String.valueOf(w);
+        int o = Integer.parseInt(y);
+        System.out.println(o);
+
+        int g = Character.getNumericValue(r);
+        System.out.println(g);
+
+
+
+
+
+
+    }
+}
