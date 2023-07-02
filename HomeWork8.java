cd345a3e98b8d3f3337fb5e05a7a659755fee93f
viachaslaufamin <119659118+viachaslaufamin@users.noreply.github.com>
2023-07-02 19:27:22 +0200

    Add files via upload

---
+++ HomeWork8.java
@@ -0,0 +1,11 @@
+package homeWork;
+
+public class HomeWork8 {
+    public static void main(String[] args) {
+        int[] i = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
+        for (int element : i) {
+            System.out.print(element + " ");
+        }
+
+    }
+}
